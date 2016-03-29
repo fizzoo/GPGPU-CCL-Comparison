9faed868ea501b3eead753f59f4e982a8323f927
fizzoo <vixirfizz@gmail.com>
2016-03-29 16:48:14 +0200

    Made tester handle multiple files

--- tester.cc
+++ tester.cc
@@ -17,100 +17,104 @@ int main(int argc, const char *argv[]) {
   cl::Program program = load_cl_program(&context, &device);
   cl::CommandQueue queue = load_queue(&context, &device);
 
-  if (argc != 2) {
+  if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
     return 0;
   } else {
     std::cerr << std::endl;
   }
 
-  std::string filename = argv[1];
-
-  iml::Image rgba_image(filename);
-  if (!rgba_image) {
-    fail("Image not loaded correctly, aborting.");
-  }
-
   {
     int err = mkdir("out", 0777);
     if (err && errno != EEXIST) {
-      fail("Couldn't creat output directory 'out'.");
+      fail("Couldn't create output directory 'out'.");
     }
   }
 
-  LabelData input(&rgba_image, rgb_above_100);
-  std::cerr << "Loaded input image '" << filename << "' into a LabelData"
-            << std::endl;
-
-  std::vector<Strategy *> strats;
-  strats.push_back(new CPUOnePass);
-  strats.push_back(new CPUUnionFind);
-  strats.push_back(new CPULinearTwoScan);
-  strats.push_back(new GPUNeighbourPropagation);
-  strats.push_back(new GPUUnionFind);
-  strats.push_back(new GPUPlusPropagation);
-  strats.push_back(new GPULineEditing);
-
-  strats[0]->copy_to(&input, &context, &program, &queue);
-  strats[0]->execute();
-  LabelData correct = strats[0]->copy_from();
-
-  // Ensures kernel and queue is ready, as they would only be created once in
-  // a usual program.
-  for (auto &strat : strats) {
-    // Make warmup not be exactly the same as input for risk of
-    // optimization. A clear image is safe for labeling.
-    LabelData warmup(input);
-    warmup.clear();
-
-    strat->copy_to(&warmup, &context, &program, &queue);
-    strat->execute();
-    strat->copy_from();
-  }
-
-  std::cerr << "(Name of file)                   -- (Name of strategy)         "
-               "      -- (Times in microseconds) -- (Times with prep/cleanup)";
-  std::cerr << std::endl;
-
-  for (auto *strat : strats) {
-    auto startwithprep = std::chrono::high_resolution_clock::now();
-    strat->copy_to(&input, &context, &program, &queue);
-
-    auto start = std::chrono::high_resolution_clock::now();
-    strat->execute();
-    auto end = std::chrono::high_resolution_clock::now();
-
-    LabelData output = strat->copy_from();
-    auto endwithprep = std::chrono::high_resolution_clock::now();
-
-    auto ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-                  .count();
-    auto mswithprep = std::chrono::duration_cast<std::chrono::microseconds>(
-                          endwithprep - startwithprep)
-                          .count();
-
-    std::cout << std::left << std::setw(32) << argv[1] << " -- "
-              << std::setw(32) << strat->name() << " -- " << std::setw(23) << ms
-              << " -- " << mswithprep << std::endl;
-
-    if (!valid_result(&output)) {
-      std::cerr << "Strategy returned an invalid labeling" << std::endl;
+  for (int i = 1; i < argc; ++i) {
+    std::string filename = argv[i];
+    
+    iml::Image rgba_image(filename);
+    if (!rgba_image) {
+      fail("Image not loaded correctly, aborting.");
     }
-    if (!equivalent_result(&correct, &output)) {
-      std::cerr << "Strategy returned an unexpected labeling." << std::endl;
+    
+    LabelData input(&rgba_image, rgb_above_100);
+    std::cerr << "Loaded input image '" << filename << "' into a LabelData"
+              << std::endl;
+    
+    std::vector<Strategy *> strats;
+    strats.push_back(new CPUOnePass);
+    strats.push_back(new CPUUnionFind);
+    strats.push_back(new CPULinearTwoScan);
+    strats.push_back(new GPUNeighbourPropagation);
+    strats.push_back(new GPUUnionFind);
+    strats.push_back(new GPUPlusPropagation);
+    strats.push_back(new GPULineEditing);
+    
+    strats[0]->copy_to(&input, &context, &program, &queue);
+    strats[0]->execute();
+    LabelData correct = strats[0]->copy_from();
+    
+    // Ensures kernel and queue is ready, as they would only be created once in
+    // a usual program.
+    for (auto &strat : strats) {
+      // Make warmup not be exactly the same as input for risk of
+      // optimization. A clear image is safe for labeling.
+      LabelData warmup(input);
+      warmup.clear();
+    
+      strat->copy_to(&warmup, &context, &program, &queue);
+      strat->execute();
+      strat->copy_from();
+    }
+    
+    std::cerr << "(Name of file)                   -- (Name of strategy)         "
+                 "      -- (Times in microseconds) -- (Times with prep/cleanup)";
+    std::cerr << std::endl;
+    
+    for (auto *strat : strats) {
+      auto startwithprep = std::chrono::high_resolution_clock::now();
+      strat->copy_to(&input, &context, &program, &queue);
+    
+      auto start = std::chrono::high_resolution_clock::now();
+      strat->execute();
+      auto end = std::chrono::high_resolution_clock::now();
+    
+      LabelData output = strat->copy_from();
+      auto endwithprep = std::chrono::high_resolution_clock::now();
+    
+      auto ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+                    .count();
+      auto mswithprep = std::chrono::duration_cast<std::chrono::microseconds>(
+                            endwithprep - startwithprep)
+                            .count();
+    
+      std::cout << std::left << std::setw(32) << filename << " -- "
+                << std::setw(32) << strat->name() << " -- " << std::setw(23) << ms
+                << " -- " << mswithprep << std::endl;
+    
+      if (!valid_result(&output)) {
+        std::cerr << "Strategy returned an invalid labeling" << std::endl;
+      }
+      if (!equivalent_result(&correct, &output)) {
+        std::cerr << "Strategy returned an unexpected labeling." << std::endl;
+      }
+    
+      // Write to file
+      iml::Image out(output.width, output.height);
+      output.copy_to_image(out.data, mod8);
+      std::string cleaninput = filename;
+      std::replace(cleaninput.begin(), cleaninput.end(), '/', '-');
+      std::string outname = "out/" + cleaninput + " - " + strat->name() + ".png";
+      iml::writepng(outname, &out);
     }
 
-    // Write to file
-    iml::Image out(output.width, output.height);
-    output.copy_to_image(out.data, mod8);
-    std::string cleaninput = argv[1];
-    std::replace(cleaninput.begin(), cleaninput.end(), '/', '-');
-    std::string outname = "out/" + cleaninput + " - " + strat->name() + ".png";
-    iml::writepng(outname, &out);
-  }
-
-  for (auto *strat : strats) {
-    delete strat;
+    std::cerr << std::endl;
+    
+    for (auto *strat : strats) {
+      delete strat;
+    }
   }
 
   return 0;
