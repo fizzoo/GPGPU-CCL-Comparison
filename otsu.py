2ca3182c903683c5e0f6d0459f6ab428a52db3cf
fizzoo <vixirfizz@gmail.com>
2016-04-05 21:01:09 +0200

    Otsu's method for thresholding into a reliably good binary image.

---
+++ otsu.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+from skimage.filters import threshold_otsu
+from skimage.io import imsave, imread
+from skimage.color import rgb2grey
+
+for index in range(1, len(argv)):
+    filename = argv[index]
+    colorimage = imread(filename)
+    image = rgb2grey(colorimage)
+    thresh = threshold_otsu(image)
+    binary = image > thresh
+    maxed = binary * 255
+    imsave(filename + ".png", maxed)
